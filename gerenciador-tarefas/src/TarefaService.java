3c6e5f664d60abac9768f76523afce6f11dd4f15
JuanGDias <juan.dias@ccc.ufcg.edu.br>
2023-08-21 23:01:17 -0300

    Código - adicionando excluir tarefa

--- gerenciador-tarefas/src/TarefaService.java
+++ gerenciador-tarefas/src/TarefaService.java
@@ -10,6 +10,14 @@ public class TarefaService {
         tarefas = new HashMap<>();
     }
 
+    public Tarefa getTarefa(String id){
+        return tarefas.get(id);
+    }
+
+    public Map<String, Tarefa> getTarefas(){
+        return tarefas;
+    }
+
     public Tarefa criarTarefa(String titulo, String descricao, String dataVencimento, String prioridade) {
 
         Tarefa tarefa = new Tarefa();
@@ -25,14 +33,14 @@ public class TarefaService {
         return tarefa;
     }
 
-    public Map<String, Tarefa> getTarefas(){
-        return tarefas;
-    }
-
     public void atualizarTarefa(Tarefa tarefa, String tituloNovo, String descricaoNova, String dataVencimentoNova, String prioridadeNova) {
         tarefa.setTitulo(tituloNovo);
         tarefa.setDescricao(descricaoNova);
         tarefa.setDataVencimento(dataVencimentoNova);
         tarefa.setPrioridade(prioridadeNova);
+    }
+
+    public void excluirTarefa(Tarefa tarefa) {
+        tarefas.remove(tarefa.getId());
     }
 }
