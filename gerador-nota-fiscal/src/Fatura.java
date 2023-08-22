6846f60cf532323dace89ecd5fe9c3d8b909822c
anapaaula <x.anapaulaa@gmail.com>
2023-08-21 21:35:27 -0300

    Refatoramento - adicionando atributos e construtor

--- gerador-nota-fiscal/src/Fatura.java
+++ gerador-nota-fiscal/src/Fatura.java
@@ -1,2 +1,13 @@
 public class Fatura {
+    private String nomeCliente;
+    private String enderecoCliente;
+    private String tipoServico;
+    private double valorFatura;
+
+    public Fatura(String nomeCliente, String enderecoCliente, String tipoServico, double valorFatura) {
+        this.nomeCliente = nomeCliente;
+        this.enderecoCliente = enderecoCliente;
+        this.tipoServico = tipoServico;
+        this.valorFatura = valorFatura;
+    }
 }
