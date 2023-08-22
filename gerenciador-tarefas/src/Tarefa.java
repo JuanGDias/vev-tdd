739d620f7369e15bbc3e8d4ca853ac5914ac945a
JuanGDias <juan.dias@ccc.ufcg.edu.br>
2023-08-21 22:49:54 -0300

    Código - criando classes tarefa e tarefaService

---
+++ gerenciador-tarefas/src/Tarefa.java
@@ -0,0 +1,41 @@
+import java.util.Objects;
+
+public class Tarefa {
+
+    private String titulo;
+    private String descricao;
+    private String dataVencimento;
+    private String prioridade;
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+}
---
+++ gerenciador-tarefas/src/TarefaService.java
@@ -0,0 +1,6 @@
+public class TarefaService {
+
+    public Tarefa criarTarefa(String titulo, String descricao, String dataVencimento, String prioridade) {
+        return null;
+    }
+}
