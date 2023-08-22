6ff933d74a3fab144fd24f89072a9913edb57d62
JuanGDias <juan.dias@ccc.ufcg.edu.br>
2023-08-21 22:52:33 -0300

    Código - adicionando atributos de tarefa

--- gerenciador-tarefas/src/TarefaService.java
+++ gerenciador-tarefas/src/TarefaService.java
@@ -1,6 +1,14 @@
 public class TarefaService {
 
     public Tarefa criarTarefa(String titulo, String descricao, String dataVencimento, String prioridade) {
-        return null;
+
+        Tarefa tarefa = new Tarefa();
+
+        tarefa.setTitulo(titulo);
+        tarefa.setDescricao(descricao);
+        tarefa.setDataVencimento(dataVencimento);
+        tarefa.setPrioridade(prioridade);
+
+        return tarefa;
     }
 }
