48c2f78b00622d5186a91d82570babc38f927d7f
JuanGDias <juan.dias@ccc.ufcg.edu.br>
2023-08-21 22:57:05 -0300

    Teste - verificando atualização de tarefa

--- gerenciador-tarefas/test/TarefaServiceTest.java
+++ gerenciador-tarefas/test/TarefaServiceTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TarefaServiceTest {
 
     private TarefaService tarefaService;
-
     @BeforeEach
     void createTarefaService(){
         tarefaService = new TarefaService();
@@ -17,7 +16,6 @@ public class TarefaServiceTest {
     @Test
     void testCriarTarefa(){
         Tarefa tarefa = tarefaService.criarTarefa("Atividade VeV", "Exercicio 2", "21-08-2023", "ALTA");
-
         assertEquals(tarefa.getTitulo(), "Atividade VeV");
         assertEquals(tarefa.getDescricao(), "Exercicio 2");
         assertEquals(tarefa.getDataVencimento(), "21-08-2023");
@@ -29,5 +27,16 @@ public class TarefaServiceTest {
         Tarefa tarefa = tarefaService.criarTarefa("Atividade 1", "Exercicio 1", "21-08-2023", "ALTA");
         Map<String, Tarefa> tarefas = tarefaService.getTarefas();
         assertEquals(1, tarefas.size());
+    }
+
+    @Test
+    void testAtualizarTarefa(){
+        Tarefa tarefa = tarefaService.criarTarefa("Atividade 1", "Quiz 1", "21-08-2023", "MEDIA");
+        tarefaService.atualizarTarefa(tarefa, "Atividade 2", "Quiz 1", "21-08-2023", "MEDIA");
+
+        assertEquals(tarefa.getTitulo(), "Atividade 2");
+        assertEquals(tarefa.getDescricao(), "Quiz 1");
+        assertEquals(tarefa.getDataVencimento(), "21-08-2023");
+        assertEquals(tarefa.getPrioridade(), "MEDIA");
     }
 }
