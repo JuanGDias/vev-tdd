72de559f4608169278fa9b15519cdc9603c2fa94
anapaaula <x.anapaulaa@gmail.com>
2023-08-21 21:52:49 -0300

    Refatoramento - adicionando envio smtp após gerar nota fiscal

--- gerador-nota-fiscal/src/GeradorNotaFiscal.java
+++ gerador-nota-fiscal/src/GeradorNotaFiscal.java
@@ -1,18 +1,25 @@
 public class GeradorNotaFiscal {
+    private Smtp smtp;
+
+    public GeradorNotaFiscal() {
+        this.smtp = new Smtp();
+
+    }
 
     public NotaFiscal gerarNotaFiscal(Fatura fatura) {
-
         NotaFiscal notaFiscal = new NotaFiscal();
         notaFiscal.setNomeCliente(fatura.getNomeCliente());
         notaFiscal.setValorNotaFiscal(fatura.getValorFatura());
         notaFiscal.setValorImposto(this.calculaImposto(fatura.getValorFatura(), fatura.getTipoServico()));
+
+        this.smtp.envia(notaFiscal);
+
         return notaFiscal;
     }
-
     public double calculaImposto(Double valorFatura, String tipoServico) {
         if (tipoServico.equals("CONSULTORIA")) {
             return valorFatura * 0.25;
-        }else if (tipoServico.equals("TREINAMENTO")) {
+        } else if (tipoServico.equals("TREINAMENTO")) {
             return valorFatura * 0.15;
         }
         return valorFatura * 0.06;
