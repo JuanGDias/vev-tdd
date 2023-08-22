a24dc13e27134ebb083a95aac43e85024f9718ca
anapaaula <x.anapaulaa@gmail.com>
2023-08-21 21:39:34 -0300

    Código - atualizando atributos da nota fiscal

--- gerador-nota-fiscal/src/GeradorNotaFiscal.java
+++ gerador-nota-fiscal/src/GeradorNotaFiscal.java
@@ -1,6 +1,10 @@
 public class GeradorNotaFiscal {
     public NotaFiscal gerarNotaFiscal(Fatura fatura) {
 
-        return null;
+        NotaFiscal notaFiscal = new NotaFiscal();
+        notaFiscal.setNomeCliente(fatura.getNomeCliente());
+        notaFiscal.setValorNotaFiscal(fatura.getValorFatura());
+        notaFiscal.setValorImposto(null);
+        return notaFiscal;
     }
 }
