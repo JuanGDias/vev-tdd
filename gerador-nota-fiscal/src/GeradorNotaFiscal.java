ebe9ae91a2961f0b97589e0878c14d11cc05624c
anapaaula <x.anapaulaa@gmail.com>
2023-08-21 21:43:55 -0300

    Código - criando calculo imposto consultoria

--- gerador-nota-fiscal/src/GeradorNotaFiscal.java
+++ gerador-nota-fiscal/src/GeradorNotaFiscal.java
@@ -1,10 +1,18 @@
 public class GeradorNotaFiscal {
+
     public NotaFiscal gerarNotaFiscal(Fatura fatura) {
 
         NotaFiscal notaFiscal = new NotaFiscal();
         notaFiscal.setNomeCliente(fatura.getNomeCliente());
         notaFiscal.setValorNotaFiscal(fatura.getValorFatura());
-        notaFiscal.setValorImposto(fatura.getValorFatura());
+        notaFiscal.setValorImposto(this.calculaImposto(fatura.getValorFatura(), fatura.getTipoServico()));
         return notaFiscal;
+    }
+
+    public double calculaImposto(Double valorFatura, String tipoServico) {
+        if (tipoServico.equals("CONSULTORIA")) {
+            return valorFatura * 0.25;
+        }
+        return 0;
     }
 }
