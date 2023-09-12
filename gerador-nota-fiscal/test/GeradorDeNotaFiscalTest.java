df0c83bd63d4777c5631a1355372d5d384be22d6
anapaaula <x.anapaulaa@gmail.com>
2023-09-11 23:14:37 -0300

    suítes de teste para o sistema gerador de nota fiscal

--- gerador-nota-fiscal/test/GeradorDeNotaFiscalTest.java
+++ gerador-nota-fiscal/test/GeradorDeNotaFiscalTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GeradorDeNotaFiscalTest {
     GeradorNotaFiscal geradorNotaFiscal;
@@ -11,7 +12,6 @@ public class GeradorDeNotaFiscalTest {
         geradorNotaFiscal = new GeradorNotaFiscal();
 
     }
-
     @Test
     void testGerarNotaFiscal(){
         Fatura fatura = new Fatura("Cliente", "Endereço", "EXEMPLO", 1000.0);
@@ -21,27 +21,107 @@ public class GeradorDeNotaFiscalTest {
         assertEquals(fatura.getValorFatura(), notaFiscal.getValorNotaFiscal());
     }
 
+    // Testes usando a técnica de Tabela de Decisões
     @Test
     void testGerarNotaFiscalTipoDeServicoConsultoria(){
-        Fatura fatura = new Fatura("Cliente A", "Endereço A", "CONSULTORIA", 1000.0);
+        Fatura fatura = new Fatura("Cliente A", "Endereço A", "CONSULTORIA", 50.0);
         NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
 
         assertEquals(fatura.getValorFatura() * 0.25, notaFiscal.getValorImposto());
+        // assertEquals(fatura.getValorFatura() * 0.25, 12.5);
     }
 
     @Test
     void testGerarNotaFiscalTipoDeServicoTreinamento(){
-        Fatura fatura = new Fatura("Cliente B", "Endereço B", "TREINAMENTO", 1000.0);
+        Fatura fatura = new Fatura("Cliente B", "Endereço B", "TREINAMENTO", 50.0);
         NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
 
         assertEquals(fatura.getValorFatura() * 0.15, notaFiscal.getValorImposto());
+        // assertEquals(fatura.getValorFatura() * 0.15, 7.5);
     }
 
     @Test
     void testGerarNotaFiscalTipoDeServicoOutro(){
-        Fatura fatura = new Fatura("Cliente C", "Endereço C", "OUTRO", 1000.0);
+        Fatura fatura = new Fatura("Cliente C", "Endereço C", "OUTRO", 50.0);
         NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
 
         assertEquals(fatura.getValorFatura() * 0.06, notaFiscal.getValorImposto());
+        // assertEquals(fatura.getValorFatura() * 0.06, 3.00);
+    }
+
+    // Testes usando a técnica de Análise de Valores Limite e Partições de Equivalência
+    @Test
+    public void testGerarNotaFiscalConsultoriaValorMinimo() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "CONSULTORIA", 0.00);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 0.00);
+    }
+    @Test
+    public void testGerarNotaFiscalConsultoriaValorProximoMinimo() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "CONSULTORIA", 0.01);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 0.0025);
+    }
+    @Test
+    public void testGerarNotaFiscalConsultoriaValorMenorQueMinimo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Fatura fatura = new Fatura("Cliente", "Endereço", "CONSULTORIA", -1);
+        });
+    }
+    @Test
+    public void testGerarNotaFiscalConsultoriaValorAleatorio() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "CONSULTORIA", 100.00);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 25.00);
+    }
+
+    @Test
+    public void testGerarNotaFiscalTreinamentoValorMinimo() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "TREINAMENTO", 0.00);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 0.00);
+    }
+    @Test
+    public void testGerarNotaFiscalTreinamentoValorProximoMinimo() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "TREINAMENTO", 0.01);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 0.0015);
+    }
+    @Test
+    public void testGerarNotaFiscalTreinamentoValorMenorQueMinimo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Fatura fatura = new Fatura("Cliente", "Endereço", "TREINAMENTO", -1);
+        });
+    }
+    @Test
+    public void testGerarNotaFiscalTreinamentoValorAleatorio() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "TREINAMENTO", 100.00);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 15.00);
+    }
+
+    @Test
+    public void testGerarNotaFiscalOutroValorMinimo() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "OUTRO", 0.00);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 0.00);
+    }
+    @Test
+    public void testGerarNotaFiscalOutroValorProximoMinimo() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "OUTRO", 0.01);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 0.0006);
+    }
+    @Test
+    public void testGerarNotaFiscalOutroValorMenorQueMinimo() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Fatura fatura = new Fatura("Cliente", "Endereço", "OUTRO", -1);
+        });
+    }
+    @Test
+    public void testGerarNotaFiscalOutroValorAleatorio() {
+        Fatura fatura = new Fatura("Cliente", "Endereço", "OUTRO", 100.00);
+        NotaFiscal notaFiscal = geradorNotaFiscal.gerarNotaFiscal(fatura);
+        assertEquals(notaFiscal.getValorImposto(), 6.00);
     }
 }
